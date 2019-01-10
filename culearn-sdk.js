0bb80471a009dae6726cff88428d6638a67f6fd4
Landon-Dou <landon.dou@outlook.com>
2019-01-10 00:43:10 -0500

    Adding retrieveData method

--- culearn-sdk.js
+++ culearn-sdk.js
@@ -28,10 +28,61 @@ function connect(login, callback) {
     })
 }
 
+function retrieveData(cookie, callback) {
+    j.setCookie(cookie, url.main)
+    request({ url: url.dashboard, jar: j }, function (error, response, html) {
+        if (!error && response.statusCode == 200) {
+            let output = {}
+            output['semesters'] = []
+
+            let $ = cheerio.load(html)
+
+            // Set 'cookie' to Cookie
+            output['cookie', cookie]
+
+            // Set 'username' to Username
+            let username = $('.logininfo').children().first()
+            output['name'] = username.text()
+
+            // Courses By Semester
+            $('.category.with_children').each(function (index, semester) {
+                // Create single Semster object
+                let objSemester = {}
+                
+                // Set 'title' to Semester's name
+                objSemester['title'] = $(semester).children().first().text()
+
+                // Create Courses Object and retrieve courses
+                let courses = $(semester).children().first().next().children()
+                objSemester['courses'] = []
+                
+                // Courses
+                courses.each(function (index, course) {
+
+                    // Create Course object and set 'title', 'url' and 'crn'
+                    let objCourse = {}
+                    objCourse['title'] = $(course).text()
+                    objCourse['url'] = $(course).children().attr('href')
+                    let crnStr = objCourse['title'].match(/\[(.*?)\]/)
+                    if(crnStr){
+                        objCourse['crn'] = crnStr[1].split(':')
+                    }
+
+                    // Push Course Object to 'courses'
+                    objSemester['courses'].push(objCourse)
+                })
+                
+                // Push Semster Object to 'semesters'
+                output['semesters'].push(objSemester)
+            })
+
+            callback(null, output)
+        }
+    })
+}
+
 function parseCookie(cookies) {
     return cookies[1].split(';')[0]
 }
-
-
 
 module.exports = connect(login, callback)
